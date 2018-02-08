c72a6b6e66696d6f6116e135c1177adc7ad64a87
yc <247067345@qq.com>
2018-02-08 17:04:17 +0800

    支持切换应用

--- app/src/main/java/net/archeryc/mockclick/MainActivity.java
+++ app/src/main/java/net/archeryc/mockclick/MainActivity.java
@@ -1,7 +1,10 @@
 package net.archeryc.mockclick;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,17 +21,20 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 参考 http://blog.csdn.net/mad1989/article/details/38109689/
+ * input keyevent 187
  *
  * @author yc
  */
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout clRoot;
     private EditText etX;
     private EditText etY;
     private EditText etTime;
     private Button btnTarget;
     private Button btnSingleClick;
     private Button btnLoopClick;
+    private Button btnSwitch;
 
 
     private ScheduledExecutorService service;
@@ -37,12 +43,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clRoot = findViewById(R.id.cl_root);
         etX = findViewById(R.id.et_x);
         etY = findViewById(R.id.et_y);
         btnTarget = findViewById(R.id.btn_target);
         btnSingleClick = findViewById(R.id.btn_single_click);
         btnLoopClick = findViewById(R.id.btn_loop_click);
         etTime = findViewById(R.id.et_time);
+        btnSwitch = findViewById(R.id.btn_switch);
 
 
         btnSingleClick.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +89,51 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "不要点啦", Toast.LENGTH_SHORT).show();
             }
         });
+
+        clRoot.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d("position", "x--->" + event.getRawX() + "y---->" + event.getRawY());
+                }
+                return false;
+            }
+        });
+
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                execShellCmd("input keyevent 187");
+                btnSwitch.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doClick("671", "423");
+                    }
+                }, 500);
+                btnSwitch.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        execShellCmd("input keyevent 187");
+                    }
+                },1000);
+                btnSwitch.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doClick("671", "423");
+                    }
+                },1500);
+                btnSwitch.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (service != null) {
+                            service.shutdown();
+                        }
+                        service = new ScheduledThreadPoolExecutor(1);
+                        service.scheduleAtFixedRate(new MySwitchTask(), 0, 1000, TimeUnit.MILLISECONDS);
+                    }
+                },2000);
+            }
+        });
     }
 
 
@@ -116,6 +169,28 @@ public class MainActivity extends AppCompatActivity {
             String targetX = etX.getText().toString();
             String targetY = etY.getText().toString();
             doClick(targetX, targetY);
+        }
+    }
+
+    private class MySwitchTask implements Runnable {
+
+        @Override
+        public void run() {
+            execShellCmd("input keyevent 187");
+            btnSwitch.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doClick("891", "421");
+                }
+            }, 500);
+//            btnSwitch.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    String targetX = etX.getText().toString();
+//                    String targetY = etY.getText().toString();
+//                    doClick(targetX, targetY);
+//                }
+//            }, 2000);
         }
     }
 
